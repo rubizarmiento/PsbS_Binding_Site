714583141da8c8ef758b51098e43c561505e5a8c
rubizarmiento <rubizarmiento@gmail.com>
2025-08-20 13:02:55 +0200

    Added contact analysis notebook

--- 5_psii/psii_psbs/run_all.sh
+++ 5_psii/psii_psbs/run_all.sh
@@ -2,7 +2,7 @@ function run_all () {
     set -e
     sim=(2 3 4 5 6 7 8)
     for n_sim in "${sim[@]}"; do
-        bash simulation.sh ${n_sim} > /dev/null 2>&1 &
+        bash simulation.sh ${n_sim} 
     done
 
 }
