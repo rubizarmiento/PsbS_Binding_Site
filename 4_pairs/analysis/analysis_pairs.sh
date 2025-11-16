3272b636da248ac939a134e9d8d1a8b56b9d1cd3
rubizarmiento <rubizarmiento@gmail.com>
2025-11-16 20:53:23 +0100

    Removed contact analysis functions

--- 4_pairs/analysis/analysis_pairs.sh
+++ 4_pairs/analysis/analysis_pairs.sh
@@ -2,48 +2,6 @@ an1=/martini/rubiz/Github/PsbS_Binding_Site/4_pairs/analysis
 odir=${an1}/csv_files
 chains=("4" "c" "r" "s")
 
-function contact_analysis_protein(){
-  for chain in "${chains[@]}"; do
-    cd ${an1}/chain_${chain}
-    gro=initial_fit.pdb
-    #xtc=test_ultrashort.xtc
-    xtc=aligned_5000ns.xtc
-    sel1="chainID A B"
-    sel2="chainID ${chain} +and not resname CLA CLB CHL *HG* PLQ PL9 *GG* *SQ* *PG* LUT VIO XAT NEO NEX W2 HOH BCR"
-    cutoff=8
-    echo "Starting contact analysis for chain ${chain}..."
-    python3 ${an1}/contact_analysis.py -cutoff "${cutoff}" -f ${gro} -traj ${xtc} -sel1 "${sel1}" -sel2 "${sel2}" -o ${odir}/contact_matrix_protein_chain${chain}_5000ns.csv -group_by1 "resids" -group_by2 "resids" > ${odir}/lifetime/contact_matrix_protein_chain${chain}_5000ns.log 2>&1 &
-  done
-}
-
-function contact_analysis_cofactors(){
-  for chain in "${chains[@]}"; do
-    cd ${an1}/chain_${chain}
-    gro=initial_fit.pdb
-    #xtc=test_ultrashort.xtc
-    xtc=aligned_5000ns.xtc
-    sel1="chainID A B"
-    sel2="resname CLA CLB CHL PLQ PL9 LUT VIO XAT NEO NEX BCR"
-    cutoff=8
-    echo "Starting contact analysis for chain ${chain}..."
-    python3 ${an1}/contact_analysis.py -cutoff "${cutoff}" -f ${gro} -traj ${xtc} -sel1 "${sel1}" -sel2 "${sel2}" -o ${odir}/contact_matrix_cofactors_chain${chain}_5000ns.csv -group_by1 "resids" -group_by2 "resnames" > ${odir}/lifetime/contact_matrix_cofactors_chain${chain}_5000ns.log 2>&1 &
-  done
-}
-
-function contact_analysis_cofactors_resids(){
-  for chain in "${chains[@]}"; do
-    cd ${an1}/chain_${chain}
-    gro=initial_fit.pdb
-    #xtc=test_ultrashort.xtc
-    xtc=aligned_5000ns.xtc
-    sel1="chainID A B"
-    sel2="resname CLA CLB CHL PLQ PL9 LUT VIO XAT NEO NEX BCR"
-    cutoff=8
-    echo "Starting contact analysis for chain ${chain}..."
-    python3 ${an1}/contact_analysis.py -cutoff "${cutoff}" -f ${gro} -traj ${xtc} -sel1 "${sel1}" -sel2 "${sel2}" -o ${odir}/contact_matrix_cofactors_resids_chain${chain}_5000ns.csv -group_by1 "resids" -group_by2 "resids" > ${odir}/lifetime/contact_matrix_cofactors_resids_chain${chain}_5000ns.log 2>&1 &
-  done
-}
-
 function lifetime_analysis_protein(){
   for chain in "${chains[@]}"; do
     cd ${an1}/chain_${chain}
